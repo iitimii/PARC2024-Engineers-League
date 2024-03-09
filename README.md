7f5f6db0966bb9481fc672a136846f2f76a14b8d
TheNoobInventor <eduobijoro@mailfence.com>
2024-03-09 21:19:48 +0100

    Update task1 launch file with argument for different routes

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # PARC2024-Engineers-League
-Pan-African Robotics Competition Engineers League category project development for the year 2024...
+Pan-African Robotics Competition Engineers League category project development for the year 2024.
+
+## Package Overview
+- [`parc_robot_bringup`](./parc_robot_bringup/) : Contains config, world, scripts and launch files to bringup PARC robot for both task 1 and task 2. 
+- [`parc_robot_description`](./parc_robot_description/) : Contains the URDF description files for PARC robot, sensors and `ros2 control`.
+- [`parc_robot_msgs`](./parc_robot_navigation/) : Contains custom messages
+
+***Work in Progress***
+
+## ROS2 Installation
+
+## Gazebo
+
+### Gazebo model setup
+
+
+### Geographiclib Installation
+
+```
+pip install geographiclib
+```
+
+### Task 1
+
+Launch task 1 (choose either `route1`, `route2` or `route3` for PARC robot initial navigation pose):
+
+```
+ros2 launch parc_robot_bringup task1_launch.py route:=route2
+```
+
+Default route is `route1`.
--- parc_robot_bringup/launch/task1_launch.py
+++ parc_robot_bringup/launch/task1_launch.py
@@ -1,13 +1,13 @@
 import os
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, TimerAction
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetLaunchConfiguration, TimerAction, OpaqueFunction
 
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 
 from launch_ros.actions import Node
-from launch_ros.substitutions import FindPackageShare
+from launch_ros.substitutions import FindPackageShare 
 
 
 def generate_launch_description():
@@ -74,6 +74,7 @@ def generate_launch_description():
         arguments=["robot_base_controller"],
     )
 
+    # Delayed start_robot_base_controller_cmd action
     start_delayed_robot_base_controller_cmd = TimerAction(
         period=4.0, actions=[start_robot_base_controller_cmd]
     )
@@ -84,10 +85,37 @@ def generate_launch_description():
         executable="spawner",
         arguments=["joint_broadcaster"],
     )
-
+    
+    # Delayed joint_broadcaster_cmd action
     start_delayed_joint_broadcaster_cmd = TimerAction(
         period=4.0, actions=[start_joint_broadcaster_cmd]
     )
+
+    # Function to create route params filename from route launch argument
+    def create_route_params_filename(context):
+        file = os.path.join(pkg_path, 'config/', 'task1_' + context.launch_configurations['route'] + '_params.yaml')
+        if os.path.exists(file):
+            return [SetLaunchConfiguration('route_params_file', file)]
+        
+    route_params_filepath = OpaqueFunction(function=create_route_params_filename)
+    
+    # Declare route launch argument
+    declare_route_cmd = DeclareLaunchArgument(
+        name="route",
+        default_value="route1",
+        description="Choose either route1, route2, or route3 to use for robot navigation through orchard",
+    )   
+
+    # Load route parameters
+    load_route_params_cmd = Node(
+        package="parc_robot_bringup",
+        executable="task1_params.py",
+        parameters=[LaunchConfiguration('route_params_file')]
+    )
+    
+    # TODO: spawn goal location
+    # TODO: spawn parc robot at an initial pose
+    # TODO: rviz
 
     # Create the launch description and populate
     ld = LaunchDescription()
@@ -95,15 +123,14 @@ def generate_launch_description():
     # Declare the launch options
     ld.add_action(declare_use_sim_time_cmd)
     ld.add_action(declare_world_cmd)
-
-    # TODO: add route launch argument
+    ld.add_action(declare_route_cmd)
 
     # Add any actions
+    ld.add_action(route_params_filepath)
+    ld.add_action(load_route_params_cmd)
     ld.add_action(start_gazebo_cmd)
     ld.add_action(start_spawner_cmd)
     ld.add_action(start_robot_state_publisher_cmd)
-    # ld.add_action(start_robot_base_controller_cmd)
-    # ld.add_action(start_joint_broadcaster_cmd)
     ld.add_action(start_delayed_robot_base_controller_cmd)
     ld.add_action(start_delayed_joint_broadcaster_cmd)
 
